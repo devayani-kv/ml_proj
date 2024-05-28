b1f32163d9825a63e9b9fa4a3233cfc77ffb27f3
devayani <kvdevayani14@gmail.com>
2024-05-28 10:30:23 +0530

    changed app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def homePage():
 @app.route('/train',methods=['GET'])  # route to train the pipeline
 def training():
     os.system("python main.py")
-    return "Training Successful, now change the route to input data and get your predictions!" 
+    return "Training Successful, now change the route to input data and get your predictionsss!" 
 
 
 @app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
